210feded25844623b08171b642881c386e9d399b
ChristopherGS <christopher.samiullah@gmail.com>
2020-10-01 22:41:51 +0100

    create fallback for auth not set

--- @coursemaker/gatsby-theme-coursemaker/auth/auth.js
+++ @coursemaker/gatsby-theme-coursemaker/auth/auth.js
@@ -2,13 +2,14 @@ import auth0 from "auth0-js"
 import { useAuth0 } from "@auth0/auth0-react";
 import { navigate } from "gatsby"
 
-const isBrowser = typeof window !== "undefined"
+const isBrowser = typeof window !== "undefined";
+
 
 const auth = isBrowser
   ? new auth0.WebAuth({
-      domain: process.env.GATSBY_AUTH0_DOMAIN,
-      clientID: process.env.GATSBY_AUTH0_CLIENTID,
-      redirectUri: process.env.GATSBY_AUTH0_CALLBACK,
+      domain: process.env.GATSBY_AUTH0_DOMAIN || "",
+      clientID: process.env.GATSBY_AUTH0_CLIENTID || "",
+      redirectUri: process.env.GATSBY_AUTH0_CALLBACK || "",
       responseType: "token id_token",
       scope: "openid profile email",
     })
